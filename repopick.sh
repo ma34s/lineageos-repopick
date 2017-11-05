a0104d4b7c3a1b299b8380fa8ad75cd94ba602a7
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2017-11-05 20:47:08 +0200

    Update inline with lineage

--- repopick.sh
+++ repopick.sh
@@ -5,27 +5,8 @@ source build/envsetup.sh
 # vendor/lineage
 repopick 188834 # repopick: Support overriding the default commits count to check
 
-# bionic
-repopick 185640 # linker: Add support for dynamic SHIM libraries
-repopick -f 185639 # Restore android_alarm.h kernel uapi header
-
 # build/make
 repopick 190430 # Allow setting the recovery density separately from the aapt config
-repopick 186687 # releasetools: don't assert device
-repopick 187332 # releasetools: squash otasigcheck support
-repopick -f 187374 # releasetools: ota_from_target_files: add FullOTA_PostValidate
-
-# external/toybox
-repopick 187155 # toybox: Add "awk" to pending.
-
-# frameworks/av
-repopick 187558-187561 # android-o-camera-hal1
-
-# frameworks/native
-repopick 185671 # native: Restore VM memory overrides
-
-# hardware/libhardware
-repopick 190451-190452
 
 # hardware/samsung
 repopick 185890 #libsec-ril: Include liblog
@@ -34,33 +15,51 @@ repopick 185890 #libsec-ril: Include liblog
 repopick 185676 # Revert "msm8974: remove from top level makefile"
 repopick 185675 # Revert "msm8974: deprecate msm8974"
 
-# hardware/qcom/audio/default
-repopick 187251 # msm8960: fixes for N
-repopick 187252 # hal: Ensure device_name is copied if empty
-repopick 187253 # msm8960: define HFP_ASM_RX_TX 24
-repopick 187254 # msm8960: mark unused variables
-
-# hardware/qcom/gps
-repopick 186455 # msm8960: Add missing liblog dependency
-repopick 186900 # msm8960: Add support for IPV6 in AGPS Interface
-repopick 186901 # msm8960: Default apn ip type to ipv4
-
 # hardware/qcom/media
 repopick 185806 # mm-video: venc: Correct a typo in variable name
-
-# packages/apps/Camera2
-repopick 188389 188518-188526 191605
 
 # system/bt
 repopick 185858 # btm_inq: fix build with BTA_HOST_INTERLEAVE_SEARCH
 
-# system/core
-repopick 185888 # utils: Threads: Handle empty thread names
-repopick 185642 # adb: Restore support for legacy f_adb interface
-repopick 187146 # init: don't reboot to bootloader on panic
+# Recovery updates
+repopick 186687; ## https://review.lineageos.org/#/c/186687
+repopick 187332 187374; ## https://review.lineageos.org/#/q/change:187332+OR+change:187374
+repopick 187155; ## https://review.lineageos.org/#/c/187155
 
-# system/sepolicy
-repopick 186244 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
-repopick 186245 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
-repopick 186246 # sepolicy: Restore support for legacy f_adb interface
-repopick 190603-190605
+# Bionic and native
+repopick -f 185639 # Restore android_alarm.h kernel uapi header
+repopick 185640; ## https://review.lineageos.org/#/c/185640
+repopick 185671; ## https://review.lineageos.org/#/c/185671
+repopick 190614 # linker: allow the linker to shim executables
+
+# Apps updates
+repopick 188389 188518-188526 191605; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Camera2+branch:lineage-15.0
+repopick 188527-188529; ## https://review.lineageos.org/#/q/project:LineageOS/android_packages_apps_Gallery2+branch:lineage-15.0
+repopick 186688; ## https://review.lineageos.org/#/c/186688
+
+# Legacy ADB
+repopick -t oreo-adb-usb-legacy; ## https://review.lineageos.org/#/q/oreo-adb-usb-legacy
+
+# System core updates
+repopick 185888 187146; ## https://review.lineageos.org/#/q/change:185888+OR+change:187146
+
+# UI updates
+repopick 193830 194226; ## https://review.lineageos.org/#/q/change:193830+OR+change:194226
+
+# Topic: android-o-camera-hal1 (188388 temporary)
+repopick -Q 'status:open+topic:android-o-camera-hal1'; ## https://review.lineageos.org/#/q/status:open+topic:android-o-camera-hal1
+repopick -f 188388; ## https://review.lineageos.org/#/c/188388
+
+# Topic: LineageOS Features
+repopick 193540; ## https://review.lineageos.org/#/c/193540
+repopick 191921 187592; ## https://review.lineageos.org/#/q/change:191921+OR+change:187592
+repopick -Q 'status:open+topic:oreo-powermenu'; ## https://review.lineageos.org/#/q/status:open+topic:oreo-powermenu
+repopick 193249 193258 191905; ## https://review.lineageos.org/#/q/change:193249+OR+change:193258+OR+change:191905
+repopick 193025 191736; ## https://review.lineageos.org/#/q/change:193025+OR+change:191736
+repopick -Q 'status:open+topic:oreo-tiles'; ## https://review.lineageos.org/#/q/status:open+topic:oreo-tiles
+repopick 193544 192490 193490 193770; ## https://review.lineageos.org/#/q/change:193544+OR+change:192490+OR+change:193490+OR+change:193770
+
+# Topic: LineageOS Lights
+repopick 193156; ## https://review.lineageos.org/#/q/change:193156
+cd $(gettop)/frameworks/base/; rm -rf .git/rebase-apply; curl https://github.com/AdrianDC/lineage_development_sony8960/commit/fbbac8c497fe3b036226a38421eb6a94f32b1ec0.patch | git am -3; cd $(gettop)/;
+repopick -Q 'status:open+topic:oreo-lights'; ## https://review.lineageos.org/#/q/status:open+topic:oreo-lights
